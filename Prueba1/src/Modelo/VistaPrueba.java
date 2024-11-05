37f5afbff22e873bb9850315d455ad9d6ec380a4
Cristiano19108 <juliorasqueso@gmail.com>
2024-11-04 18:01:35 -0600

    UPDATE Prueba Vista 311

---
+++ Prueba1/src/Modelo/VistaPrueba.java
@@ -0,0 +1,47 @@
+package Modelo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class VistaPrueba extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VistaPrueba frame = new VistaPrueba();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VistaPrueba() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Intentar");
+		btnNewButton.setBounds(179, 121, 89, 23);
+		contentPane.add(btnNewButton);
+	}
+}
